129a69ceca8825b4aab10384a6d3a936209d689c
ktrant84 <katee.trant@gmail.com>
2017-01-15 20:07:31 -0800

    Create login/logout page with functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       redirect_to user_path(@user)
     else
       render new_user_path
@@ -17,6 +18,12 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def destroy
+    @user = User.find_by(id:params[:id])
+    @user.destroy
+
+    redirect_to root_path
+  end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'sessions#new'
 
   resources :users
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   get '/youtube' => 'api#youtube'
   get '/gemo' => 'api#gemo'
   get '/twilio' => 'api#twilio'
