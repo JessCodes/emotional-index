af90c5011bba714f8d35974ea635ae6afd660ca8
ktrant84 <katee.trant@gmail.com>
2017-01-19 10:47:58 -0800

    Hide add favorite button when new favorite form is open

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,6 @@ $( document ).on('turbolinks:load', function() {
   $(".new-favorite-link").on("click",function(e){
     e.preventDefault()
     $(".new-favorite-form").show()
+    $(".new-favorite-link").hide()
   });
-
 });
